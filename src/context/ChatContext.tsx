164def56008144e6d1b1a3f0358a4fc6e6c1ec30
brestok <6234513@gmail.com>
2025-01-07 19:21:45 +0300

    test

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -9,7 +9,7 @@ const defaultMessage: Message = {
    id: "1",
    chatId: "",
    author: "bot",
-   text: "Hallo! Ik help u een kliniek te vinden die aan al uw eisen voldoet",
+   text: "Hallo! Ik help u een kliniek te vinden die aan al uw eisen voldoet.",
    datetimeInserted: new Date().toISOString(),
 };
 
