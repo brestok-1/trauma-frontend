c7613bec1b7f56f140dfa8ae2f700b12fb6279ae
Viktoria435 <124878864+Viktoria435@users.noreply.github.com>
2025-01-21 21:24:39 +0300

    fix permissions

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -3,7 +3,7 @@ import { useChat } from '../../context/ChatContext';
 import Send from '../../assets/images/Send.png';
 
 const ChatInput: React.FC = () => {
-  const { sendMessage, resetChat, entities } = useChat();
+  const { sendMessage, resetChat } = useChat();
   const [inputValue, setInputValue] = useState('');
 
   const handleSendMessage = (): void => {
@@ -24,7 +24,6 @@ const ChatInput: React.FC = () => {
     }
   };
 
-  const isDisabled = entities.length > 0;
 
   return (
     <div className="bg-[#F9F9F9] flex flex-col border border-[#E9E9E9]">
@@ -34,14 +33,12 @@ const ChatInput: React.FC = () => {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={handleKeyDown}
-        disabled={isDisabled}
       />
       <div className="flex justify-end lg:text-[16px] text-sm gap-x-2 p-2">
         <button className="bg-[#009EE3] text-white p-4" onClick={handleResetChat}>Wis chat</button>
         <button
-          className={`text-white py-4 px-6 flex items-center gap-0.5 ${isDisabled ? 'bg-[#f88648e2] cursor-not-allowed' : 'bg-[#EF6F28]'}`}
+          className={`text-white py-4 px-6 flex items-center gap-0.5 bg-[#EF6F28]`}
           onClick={handleSendMessage}
-          disabled={isDisabled}
         >
           Verstuur <img src={Send} className="size-6" />
         </button>
