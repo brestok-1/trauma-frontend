dce27e311753703dd0afbc8734725be85241bfe7
Viktoria435 <124878864+Viktoria435@users.noreply.github.com>
2025-03-01 19:27:19 +0300

    fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-   <BrowserRouter basename="/trauma-frontend">
+   <HashRouter basename="/trauma-frontend">
       <App />
-   </BrowserRouter>
+   </HashRouter>
 );
